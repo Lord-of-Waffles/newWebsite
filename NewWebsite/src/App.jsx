6a026ca4667f11d0bc3dcc5bf569aab36dd41f00
Lord-of-Waffles <ben.worton@gmail.com>
2024-10-08 09:36:55 +0300

    hjk

--- NewWebsite/src/App.jsx
+++ NewWebsite/src/App.jsx
@@ -24,15 +24,19 @@ function App() {
         <h1>Hi, welcome to my website.</h1>
         <h2>I'm Benjamin Worton, a student at the Haaga-Helia University of Applied Sciences studying software development.</h2>
         <h2>On this page you can find:</h2>
-        <button
-        //</>onClick={GoToBackground}
-        ><h3>My Background</h3></button>
-        <button
-        //onClick={GoToProjects}
-        ><h3>Projects</h3></button>
-        <button
-        //onClick={GoToContactInfo}
-        ><h3>Contact Information</h3></button>
+        <div>
+          <nav>
+            <button
+            //</>onClick={GoToBackground}
+            ><h3>My Background</h3></button>
+            <button
+            //onClick={GoToProjects}
+            ><h3>Projects</h3></button>
+            <button
+            //onClick={GoToContactInfo}
+            ><h3>Contact Information</h3></button>
+          </nav>
+        </div>
         <a href="https://github.com/Lord-of-Waffles" target="_blank"><h3>My GitHub</h3></a>
       </div>
     </>
