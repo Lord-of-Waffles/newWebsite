9c2b8da5f339cf4fbe55fb93b70f7ff604066255
Ben Worton <ben.worton@gmail.com>
2025-02-02 18:06:53 +0200

    Removed broken routing

--- NewWebsite/src/components/ProjectCards.jsx
+++ NewWebsite/src/components/ProjectCards.jsx
@@ -9,61 +9,46 @@ function Cards() {
     return (
         <>
             <Tilt tiltMaxAngleX={3} tiltMaxAngleY={3} scale={1.02}>
-                <Card style={{ position: 'relative' }}>
-                    <CardMedia
-                        component="div"
-                        style={{
-                            height: 200,
-                            width: 400,
-                            backgroundColor: '#4158D0',
-                            backgroundImage: 'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)',
-                        }}
-                    />
-                    <CardContent style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        <Typography variant="h5" component="div" style={{ color: 'white' }}>
-                            Satellite Comparison
-                        </Typography>
-                    </CardContent>
-                </Card>
+                <a href="https://lord-of-waffles.github.io/satelliteComparison/" style={{ textDecoration: 'none' }}>
+                    <Card style={{ position: 'relative' }}>
+                        <CardMedia
+                            component="div"
+                            style={{
+                                height: 200,
+                                width: 400,
+                                backgroundColor: '#4158D0',
+                                backgroundImage: 'linear-gradient(43deg, #4158D0 0%, #C850C0 46%, #FFCC70 100%)',
+                            }}
+                        />
+                        <CardContent style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                            <Typography variant="h5" component="div" style={{ color: 'white' }}>
+                                Satellite Comparison
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </a>
             </Tilt>
 
             <Tilt tiltMaxAngleX={3} tiltMaxAngleY={3} scale={1.02}>
-                <Card style={{ position: 'relative' }}>
-                    <CardMedia
-                        component="div"
-                        style={{
-                            height: 200,
-                            width: 400,
-                            backgroundColor: '#0093E9',
-                            backgroundImage: 'linear-gradient(160deg, #0093E9 0%, #80D0C7 100%)',
-                        }}
-                    />
-                    <CardContent style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        <Typography variant="h5" component="div" style={{ color: 'white' }}>
-                            To-Do List
-                        </Typography>
-                    </CardContent>
-                </Card>
+                <a href="https://lord-of-waffles.github.io/FinalTaskFrontEnd/#/" style={{ textDecoration: 'none' }}>
+                    <Card style={{ position: 'relative' }}>
+                        <CardMedia
+                            component="div"
+                            style={{
+                                height: 200,
+                                width: 400,
+                                backgroundColor: '#0093E9',
+                                backgroundImage: 'linear-gradient(160deg, #ed66ed 0%, #4a54d9 100%)',
+                            }}
+                        />
+                        <CardContent style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                            <Typography variant="h5" component="div" style={{ color: 'white' }}>
+                                Personal Trainer
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </a>
             </Tilt>
-            <Tilt tiltMaxAngleX={3} tiltMaxAngleY={3} scale={1.02}>
-                <Card style={{ position: 'relative' }}>
-                    <CardMedia
-                        component="div"
-                        style={{
-                            height: 200,
-                            width: 400,
-                            backgroundColor: '#0093E9',
-                            backgroundImage: 'linear-gradient(160deg, #ed66ed 0%, #4a54d9 100%)',
-                        }}
-                    />
-                    <CardContent style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                        <Typography variant="h5" component="div" style={{ color: 'white' }}>
-                            Personal Trainer
-                        </Typography>
-                    </CardContent>
-                </Card>
-            </Tilt>
-
         </>
     );
 }
